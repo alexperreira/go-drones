614df81c153a2261fc97056f0649433aa6efda19
Alex Perreira <alexanderperreira@gmail.com>
2022-02-17 10:46:37 -0700

    removed React-GA from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import dependencies
 import React, { useEffect } from 'react';
-import ReactGA from 'react-ga';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 // import pages
@@ -15,21 +14,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Footer from './components/UI/Footer/Footer';
 import ScrollToTop from './components/UI/ScrollToTop';
 
-function usePageViews() {
-	const TRACKING_ID = 'G-VGQSMQFFFS';
-	const location = useLocation();
-	useEffect(() => {
-		if (!window.GA_INITIALIZED) {
-			ReactGA.initialize(TRACKING_ID);
-			window.GA_INITIALIZED = true;
-		}
-		ReactGA.set({ page: location.pathname });
-		ReactGA.pageview(location.pathname);
-	}, [location]);
-}
-
 function App() {
-	usePageViews();
 	return (
 		<div>
 			<Sidebar />
